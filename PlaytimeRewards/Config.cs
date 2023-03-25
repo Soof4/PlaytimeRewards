e19403a9b9364c500708c4bac4561cdec723b08d
Soofa <127747549+Soof4@users.noreply.github.com>
2023-03-26 00:58:12 +0300

    Add files via upload

---
+++ PlaytimeRewards/Config.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaytimeRewards {
+    public class Config {
+        public int TimeInMins = 60;
+        public int[] RewardsPreHM = { 2334, 2335, 2336, 3203, 3204, 3205, 3206, 3207, 3208, 4405, 4407, 4877, 5002 };
+        public int[] RewardsHM = { 3979, 3980, 3981, 3982, 3983, 3984, 3985, 3986, 3987, 4406, 4408, 4878, 5003 };
+        public IDictionary<string, int> PlayerList = new Dictionary<string, int>();
+        public void Write() {
+            File.WriteAllText(PlaytimeRewards.path, JsonConvert.SerializeObject(this, Formatting.Indented));
+        }
+
+        public static Config Read() {
+            if (!File.Exists(PlaytimeRewards.path)) {
+                return new Config();
+            }
+            return JsonConvert.DeserializeObject<Config>(File.ReadAllText(PlaytimeRewards.path));
+        }
+    }
+}
---
+++ PlaytimeRewards/PlaytimeRewards.cs
@@ -0,0 +1,104 @@
+﻿using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+using TShockAPI.Hooks;
+
+namespace PlaytimeRewards {
+    [ApiVersion(2,1)]
+    public class PlaytimeRewards : TerrariaPlugin {
+        public override string Name => "PlaytimeRewards";
+        public override Version Version => new Version(1, 1, 0);
+        public override string Author => "Soofa";
+        public override string Description => "Gives players rewards based on how much time they've played on the server.";
+
+        public static string path = Path.Combine(TShock.SavePath + "/PlaytimeRewardsConfig.json");
+        public static Config Config = new Config();
+
+        public static int ticks = 0;
+        public PlaytimeRewards(Main game) : base(game) {
+        }
+        public override void Initialize() {
+            ServerApi.Hooks.GameUpdate.Register(this, OnGameUpdate);
+            GeneralHooks.ReloadEvent += OnReload;
+            Commands.ChatCommands.Add(new Command("pr.getreward", GetRewardCmd, "getreward", "gr"));
+            Commands.ChatCommands.Add(new Command("pr.playtime", PlayTimeCmd, "playtime", "pt"));
+
+            if (File.Exists(path)) {
+                Config = Config.Read();
+            }
+            else {
+                Config.Write();
+            }
+        }
+
+        private void PlayTimeCmd(CommandArgs args) {
+            if (Config.PlayerList.ContainsKey(args.Player.Name)) {
+                args.Player.SendInfoMessage($"You have {Config.PlayerList[args.Player.Name]} mins unused playtime.");
+            }
+            else {
+                args.Player.SendErrorMessage("You don't have any playtime.");
+            }
+        }
+
+        private void GetRewardCmd(CommandArgs args) {
+            if (args.Player.RealPlayer == false) {
+                return;
+            }
+
+            TSPlayer Player = args.Player;
+            Random rand = new Random();
+            int itemIndex;
+
+
+            if(!Config.PlayerList.ContainsKey(Player.Name)) {
+                Player.SendErrorMessage("You need to play more to get rewards.");
+                return;
+            }
+            else if (Config.PlayerList[Player.Name] < Config.TimeInMins) {
+                Player.SendErrorMessage($"You have only {Config.PlayerList[Player.Name]} mins playtime. You need to have at least {Config.TimeInMins} mins.");
+                return;
+            }
+
+
+            if(Main.hardMode) {
+                itemIndex = rand.Next(0, Config.RewardsHM.Length);
+                Player.GiveItem(Config.RewardsHM[itemIndex], 1);
+            }
+            else {
+                itemIndex = rand.Next(0, Config.RewardsPreHM.Length);
+                Player.GiveItem(Config.RewardsPreHM[itemIndex], 1);
+            }
+            Config.PlayerList[Player.Name] -= Config.TimeInMins;
+            Config.Write();
+            Player.SendSuccessMessage("You have given your reward.");
+        }
+
+        private void OnReload(ReloadEventArgs e) {
+            if (File.Exists(path)) {
+                Config = Config.Read();
+            }
+            else {
+                Config.Write();
+            }
+            e.Player.SendSuccessMessage("PlaytimeRewards plugin has been reloaded.");
+        }
+        private void OnGameUpdate(EventArgs args) {
+            if (++ticks % 3600 * Config.TimeInMins == 0) {
+                for (int i = 0; i < Main.maxPlayers; i++) {
+                    bool isFound = false;
+                    foreach (var kvp in Config.PlayerList) {
+                        if (Main.player[i].name.Equals(kvp.Key)) {
+                            Config.PlayerList[kvp.Key]++;
+                            isFound = true;
+                        }
+                    }
+                    if(!isFound && Main.player[i].name != "") {
+                        Config.PlayerList.Add(Main.player[i].name, 1);
+                    }
+                }
+                ticks = 0;
+                Config.Write();
+            }
+        }
+    }
+}
